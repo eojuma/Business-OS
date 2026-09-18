2837ccfb67337964015b7e74f0873ea4e780fed8
evajuma <evansjuma1e@gmail.com>
2026-09-18 15:35:25 +0300

    notifications: add list/count/mark-read service

--- backend/internal/modules/notifications/service.go
+++ backend/internal/modules/notifications/service.go
@@ -3,9 +3,28 @@ package notifications
 import "github.com/google/uuid"
 
 type Service interface {
-	List(uuid.UUID) ([]Notification, error)
+	List(businessID uuid.UUID, unreadOnly bool) ([]Notification, error)
+	UnreadCount(businessID uuid.UUID) (int64, error)
+	MarkRead(id, businessID uuid.UUID) error
+	MarkAllRead(businessID uuid.UUID) error
 }
+
 type service struct{ repo Repository }
 
-func NewService(repo Repository) Service                     { return &service{repo: repo} }
-func (s *service) List(id uuid.UUID) ([]Notification, error) { return s.repo.LowStock(id) }
+func NewService(repo Repository) Service { return &service{repo: repo} }
+
+func (s *service) List(businessID uuid.UUID, unreadOnly bool) ([]Notification, error) {
+	return s.repo.List(businessID, unreadOnly)
+}
+
+func (s *service) UnreadCount(businessID uuid.UUID) (int64, error) {
+	return s.repo.UnreadCount(businessID)
+}
+
+func (s *service) MarkRead(id, businessID uuid.UUID) error {
+	return s.repo.MarkRead(id, businessID)
+}
+
+func (s *service) MarkAllRead(businessID uuid.UUID) error {
+	return s.repo.MarkAllRead(businessID)
+}
